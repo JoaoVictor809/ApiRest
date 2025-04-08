ff5961bdd00c9dae6204afda813d7593d1636353
JoaoVictor809 <joaovictorleal.mello@outlook.com>
2025-04-08 11:17:42 -0300

    Update useGetData.js

--- src/hooks/useGetData.js
+++ src/hooks/useGetData.js
@@ -1,7 +1,7 @@
 const getEnem = async () => {
     try {
       const response = await api.get('/exams');
-      return response.data; // Aqui, você pode manipular os dados para adicionar IDs únicos.
+      return response.data;
     } catch (error) {
       console.log({ error });
       return error;
